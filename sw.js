64a0374a5d8d37079caa55eb7ff03669a176e4d8
havoschools <havoagenciesltd@gmail.com>
2025-04-11 20:03:52 +0000

    z

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "vendor-portal-cache-v10"; // Updated cache version
+const CACHE_NAME = "vendor-portal-cache-v11"; // Updated cache version
 const urlsToCache = [
     "/",
     "/index.html",
